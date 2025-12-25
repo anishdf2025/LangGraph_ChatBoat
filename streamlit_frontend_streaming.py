c2245679c3d7f2da20843b5c8bfb7e53b1d9177d
Anish <anishdf2025@gmail.com>
2025-12-25 12:27:24 +0000

    add the threading like chatgpt

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -13,9 +13,6 @@ for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-#{'role': 'user', 'content': 'Hi'}
-#{'role': 'assistant', 'content': 'Hi=ello'}
-
 user_input = st.chat_input('Type here')
 
 if user_input:
